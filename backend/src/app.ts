e104796ffb18f11c3105c523faf0cd0c276c29e9
Ricardo Henrique <ricardohs521@gmail.com>
2026-05-13 20:18:31 -0300

    Revert "Merge pull request #9 from ricardohsousa/feature/voluntarios-base"
    This reverts commit 055553551a608df9f31326f98ffd74a893f1eb5b, reversing
    changes made to 49f6d3fbafe38a5d4488870f7ecec735d00c1c97.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
-import { createVoluntariosModule } from './infrastructure/voluntarios/voluntarios.module';
 import { errorHandler } from './interfaces/http/middlewares/error-handler';
 import { notFoundHandler } from './interfaces/http/middlewares/not-found-handler';
 import { healthRoutes } from './interfaces/http/routes/health.routes';
 
 export const app = express();
 
-app.use(express.json());
 app.use(healthRoutes);
-app.use(createVoluntariosModule());
+app.use(express.json());
 app.use(notFoundHandler);
 app.use(errorHandler);
