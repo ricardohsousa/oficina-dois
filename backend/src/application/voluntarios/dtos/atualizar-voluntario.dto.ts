fda5c7585bc60bfd8aa22c3a78fc3aeb8cc14859
Leonardo David dos Santos <leonardo55.david@gmail.com>
2026-05-14 00:31:18 -0300

    feat: implementa atualização de voluntários #12

---
+++ backend/src/application/voluntarios/dtos/atualizar-voluntario.dto.ts
@@ -0,0 +1,9 @@
+export type AtualizarVoluntarioDto = {
+  nomeCompleto: string;
+  cpf: string;
+  dataNascimento: string;
+  email: string;
+  telefone: string;
+  endereco: string;
+  dataEntrada: string;
+};
